a59d67a62071cc2bc9778bce9a03d43cab32706e
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-04-17 08:00:11 +0200

    NEN6772: added more code (WIP).

--- src/NEN6772.c
+++ src/NEN6772.c
@@ -31,17 +31,17 @@
 
 /*!
  * \brief Unity Check on bending strength in a beam-column connection
- * (beam at the end of the column).
+ * (beam at the end of a column).
  * 
  * \image html NEN6772_11.5-1.gif 
  */
 double
 UC_11_5_1
 (
-    double M_v_s_d;
-    /*!< . */
-    double M_v_asym_u_d;
-    /*!< . */
+    double M_v_s_d,
+    /*!< Actual bending moment. */
+    double M_v_asym_u_d
+    /*!< Maximum bending capacity (asymmetrical). */
 )
 {
         return (abs (M_v_s_d) / M_v_asym_u_d);
@@ -50,17 +50,17 @@ UC_11_5_1
 
 /*!
  * \brief Unity Check on bending strength in a beam-column connection
- * (single beam branching on column).
+ * (single beam branching on a column).
  *
  * \image html NEN6772_11.5-2.gif 
  */
 double
 UC_11_5_2
 (
-    double M_v_s_d;
-    /*!< . */
-    double M_v_sym_u_d;
-    /*!< . */
+    double M_v_s_d,
+    /*!< Actual bending moment. */
+    double M_v_sym_u_d
+    /*!< Maximum bending capacity (symmetrical). */
 )
 {
         return (abs (M_v_s_d) / M_v_sym_u_d);
@@ -69,28 +69,94 @@ UC_11_5_2
 
 /*!
  * \brief Unity Check on bending strength in a beam-column connection
- * (symmetrical beams branching on column).
+ * (symmetrical beams branching on a column).
  *
  * \image html NEN6772_11.5-3.gif 
  */
 double
 UC_11_5_3
 (
-    double M_v_s_d;
-    /*!< . */
-    double V_1_k_s_d;
-    /*!< . */
-    double V_2_k_s_d;
-    /*!< . */
-    double F_v_d;
-    /*!< . */
-    double h_v;
-    /*!< . */
+    double M_v_s_d,
+    /*!< Actual bending moment. */
+    double V_1_k_s_d,
+    /*!< Actual shear force (beam 1). */
+    double V_2_k_s_d,
+    /*!< Actual shear force (beam 2). */
+    double F_v_d,
+    /*!< Maximum shear capacity. */
+    double h_v
+    /*!< Moment arm. */
 )
 {
         return (abs (((M_v_s_d / h_v) + 0.5 * (V_1_k_s_d + V_2_k_s_d)) / F_v_d));
 }
 
 
-/* EOF */
+/*!
+ * \brief Unity Check on bending strength in a beam-column connection
+ * (first of two beams branching on a column).
+ *
+ * \image html NEN6772_11.5-4.gif 
+ */
+double
+UC_11_5_4
+(
+    double M_1_v_s_d,
+    /*!< Actual bending moment (beam 1). */
+    double M_1_v_sym_u_d
+    /*!< Maximum bending capacity (symmetrical connection, beam 1). */
+)
+{
+        return (abs (M_1_v_s_d) / M_1_v_sym_u_d);
+}
 
+
+/*!
+ * \brief Unity Check on bending strength in a beam-column connection
+ * (second of two beams branching on a column).
+ *
+ * \image html NEN6772_11.5-5.gif 
+ */
+double
+UC_11_5_5
+(
+    double M_2_v_s_d,
+    /*!< Actual bending moment (beam 2). */
+    double M_2_v_sym_u_d
+    /*!< Maximum bending capacity (symmetrical connection, beam 2). */
+)
+{
+        return (abs (M_2_v_s_d) / M_2_v_sym_u_d);
+}
+
+
+/*!
+ * \brief Unity Check on bending strength in a beam-column connection
+ * (symmetrical beams branching on a column).
+ *
+ * \image html NEN6772_11.5-6.gif 
+ */
+double
+UC_11_5_6
+(
+    double M_1_v_s_d,
+    /*!< Actual bending moment (beam 1). */
+    double M_2_v_s_d,
+    /*!< Actual bending moment (beam 2). */
+    double V_1_k_s_d,
+    /*!< Actual shear force (beam 1). */
+    double V_2_k_s_d,
+    /*!< Actual shear force (beam 2). */
+    double F_v_d,
+    /*!< Actual shear force. */
+    double h_1_v,
+    /*!< Moment arm (beam 2). */
+    double h_2_v
+    /*!< Moment arm (beam 2). */
+)
+{
+        return (abs (((M_1_v_s_d / h_1_v) + (M_2_v_s_d / h_2_v) + 0.5 * (V_1_k_s_d + V_2_k_s_d)) / F_v_d));
+}
+
+
+/* EOF */
